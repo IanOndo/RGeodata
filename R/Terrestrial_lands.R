d59309e1adcebb12068b4e3cfa1123b42f74e530
Ian Ondo <I.Ondo@kew.org>
2021-04-19 15:26:00 +0100

    Added Terrestrial Ecoregions Of the World data

---
+++ R/Terrestrial_lands.R
@@ -0,0 +1,17 @@
+#' Terrestrial Lands Of the World (TLOW).
+#'
+#' A map of the Terrestrial Ecoregions Of the World (Dinerstein, 2017) with boundaries between adjacent ecoregions units dissolved (eliminated).
+#'
+#' @format A SpatialPolygonDataFrame with 144749 features and 6 variables. The variables have no special meaning (except \code{area}) but are by-products of geoprocessing:
+#' \describe{
+#'   \item{id}{...}
+#'   \item{level}{...}
+#'   \item{source}{...}
+#'   \item{parent_id}{...}
+#'   \item{sibling_id}{...}
+#'   \item{area}{area of the territory, in square kilometers}
+#' }
+#' 
+#' @source \url{https://ecoregions2017.appspot.com/}
+#' @seealso \url{https://academic.oup.com/bioscience/article/67/6/534/3102935}, \url{https://academic.oup.com/bioscience/article/51/11/933/227116}
+"terrestrial_lands"
